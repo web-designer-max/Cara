19d5d60e0c1560803fc07f12a11715b695efada4
web-designer-max <panbarasupanbarasu5@gmail.com>
2024-07-14 17:58:19 -0700

    [14-jul-2024]

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
-const close = document.getElementById('close');
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close");
+const cartBtn = document.getElementById("cartBtn");
+const addItem = document.getElementById("addItem");
 
 if (bar) {
-    bar.addEventListener('click',() => {
-        nav.classList.add('active');
-    })
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
 
 if (close) {
-    close.addEventListener('click',() => {
-        nav.classList.remove('active');
-    })
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
 }
+
+// Cart Button Click To add Number In lgBag
+let count = 0;
+cartBtn.addEventListener("click", () => {
+  count++;
+  addItem.innerText = count;
+  addItem.style.display = "inline";
+  //   addItem.style.padding='6px';
+});
